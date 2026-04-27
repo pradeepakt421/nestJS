e989a61ef573d4fa8b84773d1a54c2a7ea5fc47a
pradeepakt421 <ktpradeepa4@gmail.com>
2026-04-27 10:54:53 +0530

    2

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -3,8 +3,4 @@ import { Controller , Get} from '@nestjs/common';
 @Controller('student')
 export class StudentController {
 
-    @Get()
-    getAllStudents(){
-        return "All students..."
-    }
 }
